e2e74303d1ebae7ab1da780b44b30495931ca9c0
NotThatKindaGuy <110256896+NotThatKindaGuy@users.noreply.github.com>
2022-07-29 21:43:08 +0100

    main program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Tempo: ");
+var tempo = Console.ReadLine();
+int tempoInt = int.Parse(tempo);
+tempoInt = 60 / tempoInt;
+tempoInt = tempoInt * 1000;
+int tic = 0;
+while (true)
+{
+    //use the tempo to wait
+    System.Threading.Thread.Sleep(tempoInt);
+    Console.Beep(1000, 100);
+}
